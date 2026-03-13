8ced5d4aa9f2df6dbe0239fca00c9d18c471e442
Avinash Aade <avinash.aade.14@gmail.com>
2026-03-13 13:19:13 +0530

    headless

--- fixtures/driver_fixture.py
+++ fixtures/driver_fixture.py
@@ -4,7 +4,7 @@ from playwright.sync_api import sync_playwright
 @pytest.fixture
 def page():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False, slow_mo=500)
+        browser = p.chromium.launch(headless=True, slow_mo=500)
 
         context = browser.new_context()
         page = context.new_page()
